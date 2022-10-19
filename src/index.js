bebcc4744d49cc595bb972da79646fa230ed42d7
Ross <rosskeene196@gmail.com>
2022-10-19 07:28:46 -0500

    intial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+const App = () => {
+    return (
+        <h1>Fitness Tracker</h1>
+    )
+}
+
+
+
+const container = document.querySelector("#container");
+const root = ReactDOM.createRoot(container);
+root.render(
+ 
+    <App />
+ 
+);
