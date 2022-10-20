8554d0d4af92be8da3525541ab37d266b3a71113
Ross <rosskeene196@gmail.com>
2022-10-20 06:25:26 -0500

    links between pages complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,17 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <header>
-            <nav>
-                
+        <header className="main_container">
+            <nav className="navBar">
+                <Link to="/"> Home</Link>
+                <Link to="/activities"> Activities</Link>
+                <Link to="/routines"> Routines</Link>
+                <Link to="/my_routines"> My Routines</Link>
+                <Link to="login"> Login</Link>
+                <Link to="register"> Register</Link>
+               
             </nav>
         </header>
     )
