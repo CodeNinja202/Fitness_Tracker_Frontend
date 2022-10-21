c59b1410c3546e807c4540f788547c1a41d20619
Ross <rosskeene196@gmail.com>
2022-10-21 07:14:50 -0500

    working on Register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
-    return (
-        <h1>Register</h1>
-    )
-}
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
+ 
+return (
+    <form onSubmit={(event) => {
+    event.preventDefault()
+   
+         }}>
+      <input type="text" 
+      placeholder="Username" 
+      onChange={(event) => setUsername(event.target.value) }
+      />
 
+
+      <input type="password"
+       placeholder="Password"
+       onChange={(event) => setPassword(event.target.value)}
+        />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default Register;
