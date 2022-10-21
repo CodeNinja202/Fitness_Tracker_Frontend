84386862e3febfd69589fde42efb1b3eeec109a8
Ross <rosskeene196@gmail.com>
2022-10-20 20:16:10 -0500

    still working on routins

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ async function fetchRoutines() {
  setRoutines(results)
  
 }
-
+console.log(routines)
 
 useEffect(() => {
     fetchRoutines()
--- src/components/Routines.js
+++ src/components/Routines.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 const Routines = ({ routines }) => {
   return (
     <div className="main-div-routines">
-      {routines.map((routine) => {
-        const { creatorName, name, goal } = routine;
+      {routines.map((routine) => { 
+        const { creatorName, name, goal,id, activities} = routine;
 
         return (
-          <>
+          <Fragment key={id}>
+            
             <p><b>Created by:</b> {creatorName}</p>
             <p><b>Workout routine:</b> {name}</p>
             <p><b>Goal:</b> {goal}</p>
-           
-          </>
+            <p>Activities:{
+              activities.map(activity => {
+                const {description} = activity;
+               return (
+                <Fragment key={id}>
+                <p>Description:{description}</p>
+                
+                
+                </Fragment>
+               )
+
+
+              })
+              }
+            </p>
+          </Fragment>
         );
       })}
     </div>
