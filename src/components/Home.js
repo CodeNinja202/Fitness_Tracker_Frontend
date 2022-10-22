b57a7d1a81326e33c2630d0aa50267f43dd1e705
Ross <rosskeene196@gmail.com>
2022-10-21 21:45:12 -0500

    logout working

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,24 @@
-import React from "react";
-
+import React  from "react";
+import { Link } from 'react-router-dom';
+import { Button} from "@mui/material";
 const Home = () => {
     
     return (
+        <div>
         <h1>HOME</h1>
+        <Link style={{ textDecoration: 'none' }} to='/register'>
+        <Button
+           style={{
+            borderColor: "black",
+            height: "3rem",
+            margin: ".25rem",
+          }}
+          variant='contained'
+          type='submit'>
+          Don't have an account? Sign Up
+        </Button>
+      </Link>
+        </div>
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,45 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
 
-const Navbar = () => {
+    
+const Navbar = ({ logout, token }) => {
     return (
-        <header className="main_container">
-            <nav className="navBar">
-                <Link to="/"> Home</Link>
-                <Link to="/activities"> Activities</Link>
-                <Link to="/routines"> Routines</Link>
-                <Link to="/my_routines"> My Routines</Link>
-                <Link to="login"> Login</Link>
-                <Link to="register"> Register</Link>
-               
-            </nav>
-        </header>
-    )
-}
-
+      <header>
+        <nav>
+        <Link to="/"> Home</Link>
+        <Link to="/activities"> Activities</Link>
+        <Link to="/routines"> Routines</Link>
+  
+          
+  
+          {token ? (
+            <>
+              
+              <Link to="/my_routines"> My Routines</Link>
+              <Link
+                style={{ textDecoration: "none" }}
+                to="/"
+                onClick={() => logout()}
+              >
+                <Button
+                  style={{
+                   
+                    borderColor: "black",
+                    
+                  }}
+                  variant="outlined"
+                >
+                  Logout
+                </Button>
+              </Link>
+            </>
+            ) : (
+                <Link to="/login"> Login</Link>
+          )}
+        </nav>
+      </header>
+  );
+};
 
 export default Navbar;
